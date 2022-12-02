54116bc192be838286dbbba8914416d07b9cdf0d
Alyona <alyonushka489@gmail.com>
2022-12-02 15:07:00 +0300

    Выполненное ДЗ 2.5

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
+import transport.*;
+
 public class Main {
     public static void main(String[] args) {
+        Car bmw = new Car("BMW", "X-5", 2, TypeOfBody.CROSSOVER);
+        Car audi = new Car("Audi", "A-8", 1.7, TypeOfBody.SEDAN);
+        Car vesta = new Car("Lada", "Vesta", 1.6, TypeOfBody.SEDAN);
+        Car megane = new Car("Renault", "Megane", 1.8, TypeOfBody.SEDAN);
+        System.out.println(bmw);
+        System.out.println(audi);
+        System.out.println(vesta);
+        System.out.println(megane);
+        audi.pitstop();
+        System.out.println();
+        Truck maz = new Truck("МАЗ", "535", 5.9, Weight.N1);
+        Truck hyundai = new Truck("Hyundai", "Mighty", 7, Weight.N2);
+        Truck kamaz = new Truck("КамАЗ", "65207", 8.2, Weight.N3);
+        Truck isuzu = new Truck("ISUZU", "ELF", 8.5, Weight.N1);
+        System.out.println(maz);
+        System.out.println(hyundai);
+        System.out.println(kamaz);
+        System.out.println(isuzu);
+        kamaz.maxSpeed();
+        System.out.println();
+        Bus daewoo = new Bus("Daewoo", "BC212MA", 2.1, Capacity.SMALL);
+        Bus nefaz = new Bus("НЕФАЗ", "5299-32", 3.4, Capacity.AVERAGE);
+        Bus mercedes = new Bus("Mercedes", "Benz Conecto", 5, Capacity.EXTRA_LARGE);
+        Bus volvo = new Bus("Volvo", "9900", 4.7, Capacity.LARGE);
+        System.out.println(daewoo);
+        System.out.println(nefaz);
+        System.out.println(mercedes);
+        System.out.println(volvo);
+        daewoo.bestTime();
     }
 }
